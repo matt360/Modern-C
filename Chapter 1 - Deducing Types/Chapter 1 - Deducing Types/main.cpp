2e25e9adf4319808074c6f6a0343c33b2fd18be2
Matthew Wallace <1502616@abertay.ac.uk>
2018-01-26 15:47:05 +0000

    arraySize

--- Chapter 1 - Deducing Types/Chapter 1 - Deducing Types/main.cpp
+++ Chapter 1 - Deducing Types/Chapter 1 - Deducing Types/main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <array>
 
 const char name[] = "J.P. Briggs";
 const char * ptrToName = name;
 
 template<typename T>
-void f(T param) {
-};
+void f(T param) {};
 
 template<typename T, std::size_t N>
-constexpr std::size_t arraySize(T(&)[N]) noexcept
-{
+constexpr std::size_t arraySize(T(&)[N]) noexcept  // as for arraySize being declared nonexcept, 
+{                                                  // that's to help compilers generate better code.
 	return N;
 }
 
 int main()
 {
 	f(name);
+
+	int keyVals[] = { 1, 3, 7, 9, 11, 22, 35 };     // keyVals has 7 elements
+
+	int mappedVals[arraySize(keyVals)];             // so does mappedVals
+
+	std::array<int, arraySize(keyVals)> mappedVals; // mappedVals' size is 7
 
 	return 0;
 }
