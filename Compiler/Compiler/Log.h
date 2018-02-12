2a72bd3c129c48450f0495b23e825f1f5bba7b4b
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-12 21:20:39 +0000

    update

---
+++ Compiler/Compiler/Log.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//inline void Log(const char* message)
+//{
+//	std::cout << message << std::endl;
+//}
+//
+//static void Log(const char* message)
+//{
+//	std::cout << message << std::endl;
+//}
+
+void Log(const char* message);
