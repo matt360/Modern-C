f8a12854527070b4e859dce353967529e830f753
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-21 18:03:14 +0000

    references

--- Classes/Pointers/Main.cpp
+++ Classes/Pointers/Main.cpp
@@ -7,6 +7,11 @@ It is an integer that holds a memory address.
 (Types are just some kind of fiction, that we've created to make our lives easier)
 A pointer, for all types, is just an integer that holds that memory address.
 That's all that it is.
+
+In 32-bit application a memory address is 32 bits
+
+endianess - referring to the orientation of a computer system, application, 
+or network design with respect to the placement of the most significant bit, digit, or byte in a coding scheme.
 */
 
 // CODE
@@ -23,6 +28,8 @@ int main()
 	// in the actual source code, to make our lives easier, we can use types with pointers.
 	// The type does not change what a pointer is. A pointer is just a memory address, it's just an integer.
 
+	// STACK ALLOCATION
+
 	//int var = 8;
 	//void* ptr = &var;
 	//void* ptr = nullptr; // zero is not a valid memory address
@@ -36,6 +43,22 @@ int main()
 	types can be useful for manipulation of the memory, so if I want to read and write to the memory, types can help us out,
 	because the compiler will now that the integer is 4 bytes, so when I try to set the value there, it will set 4 bytes of memory.
 	*/
+
+	// HEAP ALLOCATION
+	
+	// allocating 8 bytes of memory for us and is returing a pointer to the beginning of that block of memory
+	char* buffer = new char[8]; // asking to allocate 8 bytes of memory
+	// char (1 byte)   // asking for 8 bytes of memory
+
+	// memset - fills a block of memory with data that we specify
+	// memset(pointer to the beginning of the block of memory, value, size)
+	memset(buffer, 0, 8);
+
+	// a pointer to a pointer
+	char** ptr = &buffer;
+
+    // we used an array to allocate it so we should use an array the delete keyword with the array operator
+	delete[] buffer;
 
 	std::cin.get();
 }
---
+++ Classes/References/Main.cpp
@@ -0,0 +1,51 @@
+/*
+Refernces are just pointers in disguise.
+It's just syntax sugar on top of poitners to make it a little bit easier to read and a little bit easier to follow.
+
+A reference is essentaily exactly what it sounds like - it's a way for us to reference an exisitng variable.
+
+Unlike a pointer, where you could create a new pointer variable and then set it equal to nullptr or 0 or NULL,
+you can't do this with references.
+
+References are not new variables, they have to reference an already existing variable.
+
+References don't reallt occupy memory, they don't really have storage, like a typicall variable, becasue what they are, 
+instead is a reference to a variable.
+*/
+
+
+// CODE
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
+
+//void Increment(int* value)
+//{
+//	// this vould increment the pointer first and then dereference
+//	//*value++;
+//
+//	// dereference first and then increment the value
+//	(*value)++;
+//}
+
+void Increment(int& value)
+{
+	// this vould increment the pointer first and then dereference
+	//*value++;
+
+	// dereference first and then increment the value
+	value++;
+}
+
+int main()
+{
+	int a = 5;
+	int& ref = a; // reference (alias)
+	ref = 2; // we can use 'ref' as if it was 'a'
+
+	Increment(a);
+
+	LOG(a);
+
+	std::cin.get();
+}
