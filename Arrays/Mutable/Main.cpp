8044ab9c7e13bbe7a2c345c52027ba5633a361d2
Matthew Wallace <1502616@abertay.ac.uk>
2018-02-21 23:14:56 +0000

    member initializer list

--- Arrays/Mutable/Main.cpp
+++ Arrays/Mutable/Main.cpp
@@ -7,12 +7,26 @@
 class Entity
 {
 private:
+	//mutable int m_DebugCount = 0;
 	std::string m_Name;
-	mutable int m_DebugCount = 0;
+	int m_Score;
 public:
+	Entity() : 
+		// member initializer list must keep the same order as the member declarations, it also prevents from creating an object twice
+		m_Name("Unknown"), m_Score(0) 
+	{
+		//m_Name = "Unknown";
+	}
+
+	Entity(const std::string& name) :
+		m_Name(name)
+	{
+		//m_Name = name;
+	}
+
 	const std::string& GetName() const
 	{
-		m_DebugCount++;
+		//m_DebugCount++;
 		return m_Name;
 	}
 };
@@ -22,7 +36,7 @@ int main()
 	const Entity e;
 	e.GetName(); // because the methods are declared as const, they can be called on a const class declaration
 
-	int x = 8;
+	float x = 8;
 
 	// lambda - a throwaway function that can be assing to a variable
 	auto f = [=]() mutable
@@ -35,8 +49,13 @@ int main()
 	f();
 
 	// x is still 8
-	std::cout << x << std::endl;
+	std::cout << std::scientific << x << std::endl;
 
-	//std::scientific
+	Entity e0;
+	std::cout << e0.GetName() << std::endl;
+
+	Entity e1("Cherno");
+	std::cout << e1.GetName() << std::endl;
+	
 	std::cin.get();
 }
