3d3bd901af3947e668a451c88c4e74fd69357e0b
Matthew Wallace <1502616@abertay.ac.uk>
2018-03-01 13:58:34 +0000

    explicit implicit

--- MoreC/ImplicitExplicit/src/Main.cpp
+++ MoreC/ImplicitExplicit/src/Main.cpp
@@ -13,11 +13,44 @@ public:
 
 	Entity(int age) :
 		m_Name("unknows"), m_Age(age) {}
+
+	/*
+	// explicit - block implicit conversion of the constructor
+	explicit Entity(int age) :
+		m_Name("unknows"), m_Age(age) {}
+	*/
 };
+
+void PrintEntity(const Entity& entity)
+{
+	// Printing
+}
 
 int main()
 {
-	Entity a("Cherno"); // Entity a = Entity("Cherno");
-	Entity b(22);       // Entity b = Entity(22);
+	//Entity a("Cherno"); 
+	//Entity a = Entity("Cherno");
+	//Entity a = "Cherno";
+	//Entity b(22);       
+	//Entity b = Entity(22);
 
+	// Implicit conversion
+	Entity b = 22;
+	// best practice
+	Entity c(22); 
+
+	// Implicit conversion - Entity has a constructor that takes an int so it's possible to construct an object and pass it to the function
+	PrintEntity(22);
+	//PrintEntity("Cherno"); error
+	PrintEntity(std::string("Cherno")); // implicit conversion into a string
+	PrintEntity(Entity("Cherno"));      // implicit conversion into an Entity, and then into a string
+
+	// Explicit
+	Entity d(22);
+	Entity e = (Entity)22;
+	Entity f = Entity(22);
+
+	PrintEntity(Entity("Cherno"));
+
+	std::cin.get();
 }
