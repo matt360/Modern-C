712880cacca72ca3a481bfbaca6ec1b45828b855
Matthew Wallace <1502616@abertay.ac.uk>
2018-03-01 12:48:00 +0000

    loops

--- Loops/Loops/src/Main.cpp
+++ Loops/Loops/src/Main.cpp
@@ -2,5 +2,45 @@
 
 int main()
 {
+	// for loop
+	int i = 0;
+	bool condition = true;
+	for (; condition; )
+	{
+		std::cout << "Hello World!" << std::endl;
+		i++;
+		if (!(i < 5))
+			condition = false;
+	}
 
+	std::cout << "======================" << std::endl;
+
+	// while loop
+	i = 0;
+	while (i < 5)
+	{
+		std::cout << "Hello World!" << std::endl;
+		i++;
+	}
+
+	std::cout << "======================" << std::endl;
+
+	// do while
+	condition = false;
+
+	// will execute at least once
+	do
+	{
+		std::cout << "Hello World!" << std::endl;
+	} while (condition);
+
+	std::cout << "======================" << std::endl;
+
+	// while loop will not execute at all
+	while (condition)
+	{
+		std::cout << "Hello World!" << std::endl;
+	}
+		
+	std::cin.get();
 }
