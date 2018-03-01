3ee9f7f897a1b5dd4552f5d26b1a1615240b9266
Matthew Wallace <1502616@abertay.ac.uk>
2018-03-01 18:51:02 +0000

    smart pointers - use them

--- MoreC/SmartPointers/src/Main.cpp
+++ MoreC/SmartPointers/src/Main.cpp
@@ -5,7 +5,7 @@ std::shared_ptr - works via reference counting - a practice where you keep track
 And as soon as that reference count reaches zero that's when it gets deleted so as an example I create one shared pointer, I then
 create another shared pointer and copy that and my ref counter is now 2, '1' for the first one and '1' for the second one (two refs),
 when the first one dies my ref count is 1, when the last one dies my ref count is 0, so the memory gets freed.
-std::weak_ptr -
+std::weak_ptr - can assign a shared_ptr to it but it won't increase the ref count. Good for creating references and e.g.: to check if the object is still alive.
 
 */
 
@@ -51,10 +51,32 @@ int main()
 		// shared pointer
 		std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>(); // pref way - one construction for both control block and object
 		// std::shared_ptr<Entity> sE0(new Entity); // unprefway - two construcitons, one for the control block and one for the object
+		std::shared_ptr<Entity> e0 = sharedEntity; // pointer reassignment works just fine
 
 		entity->Print();
 	}
 
+	{
+		std::shared_ptr<Entity> e0;
+		{
+			std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>(); 
+			std::weak_ptr<Entity> weakPointer = sharedEntity; // this won't increase the ref count (assigning shared ptr to weak ptr)
+			std::shared_ptr<Entity> sharedEntity0 = sharedEntity; // this will increase the ref count (assigning shared ptr to shared ptr)
+			e0 = sharedEntity;
+		}
+	} // e0 gets destroyed
+
+	{
+		std::weak_ptr<Entity> e0;
+		{
+			std::shared_ptr<Entity> sharedPoitner = std::make_unique<Entity>();
+			e0 = sharedPoitner;
+		} // sharedPointer gets destroyed, e0 points to an invalid object
+	}
+
+	{
+		std::shared_ptr<Entity> sharedPointer = std::make_unique<Entity>();
+	}
 
 	std::cin.get();
 }
