524fba6a1eb04e434b2fefbddb2f4d85e942c061
matt360 <matzaremba@icloud.com>
2018-03-19 11:13:50 +0000

    copying and copy constructors

---
+++ MoreC/CopyingAndCopyConstructors/src/Main.cpp
@@ -0,0 +1,21 @@
+/*
+COPYING AND COPY CONSTRUCTORS
+Copying refers to copying data, copying memory, when we literaly want to copy one object, or primitive, or a piece of data, 
+from one place to another, that we literally have to copied of it. A lot of the time we want to copy objects so we can modify them 
+in certain ways, if we can avoid unnecessary copying, because we just want to read the value, we want to actually modify existing object,
+we want to do it, because copying takes time.
+Copying can be both a really use thing we actally need to make our programme work in a way we want it to, but on the flip side,
+unnecessary copying is bad, and we want to avoid that, as much as possible, because it wastes performance.
+So understanding how copying actually works in C++ and how to get it to work and also how to avoid 'getting it to work', and to avoid copying
+when you don't want a copy is very important to understanding the language and being able to write C++ code efficiently and correctly.
+*/
+
+// CODE
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cin.get();
+}
