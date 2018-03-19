1706eb33bd44fe0d16cc620c7fadd9029ea93505
matt360 <matzaremba@icloud.com>
2018-03-19 10:23:18 +0000

    this

--- MoreC/this/src/Main.cpp
+++ MoreC/this/src/Main.cpp
@@ -1,5 +1,10 @@
-// Object lifetime
+// this
 /*
+'this' keyword is only accesible to us through a member function (Member function - a function that belongs to a class - so a method).
+And inside a method we can reference 'this' - and what 'this' is, is a pointer to a current object instance that the method belongs to.
+So in C++ we can run a method, a non-static method, and in order to call that method we need to first instantiate an object and then call
+the method; so the method has to be called with a valid object, and 'this', the 'this' keyword is a pointer to that object.
+'this' is really important to how methods work in general. 
 */
 
 // CODE
