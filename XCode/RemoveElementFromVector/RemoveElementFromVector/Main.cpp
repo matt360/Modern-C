31aeddccb41ecf1ec400cd448b53a3b099a649bd
Mateusz Zaremba <matzaremba@icloud.com>
2018-04-07 14:27:42 +0200

    added: RemoveElementFromVector removed: NewProject

---
+++ XCode/RemoveElementFromVector/RemoveElementFromVector/Main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RemoveElementFromVector
+//
+//  Created by Mateusz Zaremba on 07/04/2018.
+//  Copyright © 2018 Mateusz Zaremba. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
