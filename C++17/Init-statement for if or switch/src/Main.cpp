303b6fea053820fa89cd53ba460c07b21de78b8e
matt360 <matzaremba@icloud.com>
2018-04-19 13:33:16 +0200

    C++17: more examples

--- C++17/Init-statement for if or switch/src/Main.cpp
+++ C++17/Init-statement for if or switch/src/Main.cpp
@@ -4,10 +4,22 @@ int main()
 {
 	const std::string myString = "My Hello World Wow";
 
-	if (const auto it = myString.find("World"); it != std::string::npos)
-		std::cout << "World found on the " << it << "th position" << std::endl;
+	// C++17
+	if (const auto it = myString.find(" "); it != std::string::npos)
+		std::cout << "Found a space on the " << it << "th place" << std::endl;
 	else
-		std::cout << "Nothing found!" << std::endl;
+		std::cout << "Space not found!" << std::endl;
+
+	// C++
+	const auto it = myString.find_last_of(" ");
+	if (it != std::string::npos)
+		std::cout << "Last space found! Index number: " << it << std::endl;
+	else
+		std::cout << "Space not found!" << std::endl;
+
+	// C++ 17
+	if (const auto it = myString.find_first_of(" "); it != std::string:npos)
+		std::cout << "First space found! Index number: " << it << std::endl;
 
 	std::cin.get();
 }
