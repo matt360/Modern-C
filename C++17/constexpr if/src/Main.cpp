c0979f6cc9696ca6ff90e423f46938725ba727a2
matt360 <matzaremba@icloud.com>
2018-04-19 15:28:01 +0200

    C++17 constexpr if

---
+++ C++17/constexpr if/src/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+struct S
+{
+	S() {}
+	S(int N, std::string S, float D) 
+		: n(N), s(S), d(D) {}
+
+	int n;
+	std::string s;
+	float d;
+};
+
+template <std::size_t I>
+auto& get(S& s)
+{
+	if constexpr (I == 0)
+		return s.n;
+	else if constexpr (I == 1)
+		return s.s;
+	else if constexpr (I == 2)
+		return s.d;
+}
+
+
+int main()
+{
+	S s(1, "Cat", 2.0);
+	std::cout << get<0>(s);
+	
+	std::cin.get();
+}
