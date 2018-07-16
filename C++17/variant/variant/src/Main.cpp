558ebba8a4df1a90b23f1e0bdf2f9da75a92d602
matt360 <matzaremba@icloud.com>
2018-07-16 15:44:40 +0200

    union - before delete

--- C++17/variant/variant/src/Main.cpp
+++ C++17/variant/variant/src/Main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <variant>
-
 // source code: https://www.bfilipek.com/2018/06/variant.html
 
 union SuperFloat
@@ -19,9 +17,32 @@ int RawExponent(SuperFloat f)
 	return (f.i >> 23) & 0xFF;
 }
 
+class VECTOR3D
+{
+public:
+	// operations, etc...
+
+	union
+	{
+		float m[3];
+
+		struct
+		{
+			float x, y, z;
+		};
+	};
+};
+
+
+
 int main()
 {
-	
+	VECTOR3D v;
+	// same effect
+	v.m[0] = 1.0f;
+	v.x = 1.0f;
+
+
 
 	std::cin.get();
 }
