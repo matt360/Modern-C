3c74530eefa328e0379e87c38e151a6aeef84313
matt360 <matzaremba@icloud.com>
2018-07-16 16:21:24 +0200

    variant

--- C++17/variant/variant/src/Main.cpp
+++ C++17/variant/variant/src/Main.cpp
@@ -1,48 +1,67 @@
+#include <string>
 #include <iostream>
-// source code: https://www.bfilipek.com/2018/06/variant.html
+#include <variant>
 
-union SuperFloat
+// souce code: https://www.bfilipek.com/2018/06/variant.html
+
+struct SampleVisitor
 {
-	float f;
-	int i;
+	void operator()(int i) const {
+		std::cout << "int: " << i << "\n";
+	}
+	void operator()(float f) const {
+		std::cout << "float: " << f << "\n";
+	}
+	void operator()(const std::string& s) const {
+		std::cout << "string: " << s << "\n";
+	}
 };
-
-int RawMantissa(SuperFloat f)
-{
-	return f.i & ((1 << 23) - 1);
-};
-
-int RawExponent(SuperFloat f)
-{
-	return (f.i >> 23) & 0xFF;
-}
-
-class VECTOR3D
-{
-public:
-	// operations, etc...
-
-	union
-	{
-		float m[3];
-
-		struct
-		{
-			float x, y, z;
-		};
-	};
-};
-
-
 
 int main()
 {
-	VECTOR3D v;
-	// same effect
-	v.m[0] = 1.0f;
-	v.x = 1.0f;
+	std::variant<int, float, std::string> intFloatString;
+	static_assert(std::variant_size_v<decltype(intFloatString)> == 3);
 
+	// default initialized to the first alternative, should be 0
+	std::visit(SampleVisitor{}, intFloatString);
 
+	// index will show the currently used 'type'
+	std::cout << "index = " << intFloatString.index() << std::endl;
+	intFloatString = 100.0f;
+	std::cout << "index = " << intFloatString.index() << std::endl;
+	intFloatString = "hello super world";
+	std::cout << "index = " << intFloatString.index() << std::endl;
+
+	// try with get_if:
+	if (const auto intPtr(std::get_if<int>(&intFloatString)); intPtr)
+		std::cout << "int!" << *intPtr << "\n";
+	else if (const auto floatPtr(std::get_if<float>(&intFloatString)); floatPtr)
+		std::cout << "float!" << *floatPtr << "\n";
+
+	if (std::holds_alternative<int>(intFloatString))
+		std::cout << "the variant holds an int!\n";
+	else if (std::holds_alternative<float>(intFloatString))
+		std::cout << "the variant holds a float\n";
+	else if (std::holds_alternative<std::string>(intFloatString))
+		std::cout << "the variant holds a string\n";
+
+	// try/catch and bad_variant_access
+	try
+	{
+		auto f = std::get<float>(intFloatString);
+		std::cout << "float! " << f << "\n";
+	}
+	catch (std::bad_variant_access&)
+	{
+		std::cout << "our variant doesn't hold float at this moment...\n";
+	}
+
+	// visit:
+	std::visit(SampleVisitor{}, intFloatString);
+	intFloatString = 10;
+	std::visit(SampleVisitor{}, intFloatString);
+	intFloatString = 10.0f;
+	std::visit(SampleVisitor{}, intFloatString);
 
 	std::cin.get();
 }
