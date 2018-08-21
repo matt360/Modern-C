446df171cdafde8765eabc3af35fc6091144f3eb
matt360 <matzaremba@icloud.com>
2018-08-21 16:05:46 +0200

    pure virtual destructor

--- 3rdEditionEffectiveC++/pure virtual destructor/Main.cpp
+++ 3rdEditionEffectiveC++/pure virtual destructor/Main.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <string>
 #include <vector>
 
 /*
 Occasionally it can be convenient to give a class a pure virtual
 destructor. Recall that pure virtual functions result in abstract classes
-— classes that can’t be instantiated (i.e., you can’t create objects of
-that type). Sometimes, however, you have a class that you’d like to be
-abstract, but you don’t have any pure virtual functions. What to do?
+â€” classes that canâ€™t be instantiated (i.e., you canâ€™t create objects of
+that type). Sometimes, however, you have a class that youâ€™d like to be
+abstract, but you donâ€™t have any pure virtual functions. What to do?
 Well, because an abstract class is intended to be used as a base class,
 and because a base class should have a virtual destructor, and
 because a pure virtual function yields an abstract class, the solution
 is simple: declare a pure virtual destructor in the class you want to be
-abstract. Here’s an example:
+abstract. Hereâ€™s an example:
 */
 
-// This class has a pure virtual function, so it’s abstract, and it has a virtual
-// destructor, so you won’t have to worry about the destructor problem.
+// This class has a pure virtual function, so itâ€™s abstract, and it has a virtual
+// destructor, so you wonâ€™t have to worry about the destructor problem.
 // There is one twist, however: you must provide a definition for the pure virtual destructor ->
 class AWOV                  // AWOV = "Abstract w/o Virtuals"
 {
@@ -24,17 +24,19 @@ public:
 	virtual ~AWOV() = 0;    // declare pure virtual destructor
 };
 
-// The way destructors work is that the most derived class’s destructor
+// The way destructors work is that the most derived classâ€™s destructor
 // is called first, then the destructor of each base class is called.
-// Compilers will generate a call to ~AWOV from its derived classes’ destructors,
-// so you have to be sure to provide a body for the function. If you don’t,
+// Compilers will generate a call to ~AWOV from its derived classesâ€™ destructors,
+// so you have to be sure to provide a body for the function. If you donâ€™t,
 // the linker will complain.
 AWOV::~AWOV() {}            // -> definition of pure virtual dtor
 
-int main()
-{
+// The rule for giving base classes virtual destructors applies only to
+// polymorphic base classes â€” to base classes designed to allow the
+// manipulation of derived class types through base class interfaces.
 
-
-
-	std::cin.get();
-}
+// Things to Remember
+// âœ¦ Polymorphic base classes should declare virtual destructors. If a
+// class has any virtual functions, it should have a virtual destructor.
+// âœ¦ Classes not designed to be base classes or not designed to be used
+// polymorphically should not declare virtual destructors.
