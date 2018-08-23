2ed6da51e935dc297052a32310dcd0ff888ef6a0
matt360 <matzaremba@icloud.com>
2018-08-23 11:51:19 +0200

    wrong init is being called

--- 3rdEditionEffectiveC++/virtual function in constructor/Main.cpp
+++ 3rdEditionEffectiveC++/virtual function in constructor/Main.cpp
@@ -5,7 +5,9 @@
 class Transaction {							   // base class for all transactions
 public:
 	Transaction();
-	virtual void logTransaction() const = 0;   // make type-dependent log entry
+	virtual void logTransaction() const {      // make type-dependent log entry
+		std::cout << "Transaction" << std::endl;
+	};
 	void init() {
 		logTransaction();                      // as final action, log this transaction
 	};
@@ -20,15 +22,19 @@ Transaction::Transaction()		               // implementation of base class actor
 	init();
 }
 
-class BuyTransaction : public Transaction {    // derived class
+class BuyTransaction : public Transaction {            // derived class
 public:
-	virtual void logTransaction() const {};    // how to log transactions of this type
+	virtual void logTransaction() const {
+		std::cout << "BuyTransaction" << std::endl;    // how to log transactions of this type
+	};
 	// ...
 };
 
 class SellTransaction : public Transaction {   // derived class
 public:
-	virtual void logTransaction() const {};    // how to log transactions of this type
+	virtual void logTransaction() const {
+		std::cout << "SellTransaction" << std::endl; // how to log transactions of this type
+	};
 	// ...
 };
 
