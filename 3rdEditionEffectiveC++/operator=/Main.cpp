59e6c77edb550c78ebcb152b91ee84927543408d
matt360 <matzaremba@icloud.com>
2018-08-24 13:34:47 +0200

    operator=

---
+++ 3rdEditionEffectiveC++/operator=/Main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Bitmap {
+
+};
+
+class Widget{
+	void swap(Widget& rhs) {         // exchange *this's and rhs's data;
+	};
+
+	Bitmap *pb;
+};
+
+// The traditional way to prevent this error is to check for assignment to
+// self via an identity test at the top of operator=:
+Widget& Widget::operator=(const Widget& rhs)
+{
+	if (this == &rhs) return *this;   // identity test: if a self-assignment, do nothing
+}
+
+// Happily, making operator= exception - safe typically renders it selfassignment -
+// safe, too.As a result, it’s increasingly common to deal
+// with issues of self - assignment by ignoring them, focusing instead on
+// achieving exception safety.Item 29 explores exception safety in depth,
+// but in this Item, it suffices to observe that in many cases, a careful
+// ordering of statements can yield exception - safe(and self - assignmentsafe)
+// code.Here, for example, we just have to be careful not to delete
+// pb until after we’ve copied what it points to :
+Widget& Widget::operator=(const Widget& rhs)
+{
+	Bitmap *pOrig = pb;
+	pb = new Bitmap(*rhs.pb);
+	delete pb;
+}
+
+// An alternative to manually ordering statements in operator= to make
+// sure the implementation is both exception - and self - assignment - safe
+// is to use the technique known as “copy and swap.” This technique is
+// closely associated with exception safety, so it’s described in Item 29.
+// However, it’s a common enough way to write operator= that it’s worth
+// seeing what such an implementation often looks like :
+Widget& Widget::operator=(const Widget& rhs)
+{
+	Widget temp(rhs);            // make a copy of rhs's data
+	swap(temp);                  // swap *this's data with the copy's
+	return *this;
+}
+
+// A variation on this theme takes advantage of the facts that(1) a class’s
+// copy assignment operator may be declared to take its argument by
+// value and (2) passing something by value makes a copy of it
+Widget& Widget::operator=(Widget rhs)
+{
+	swap(rhs);
+	return *this;
+}
+
+int main()
+{
+
+
+
+	std::cin.get();
+}
