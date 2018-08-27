22782549d820c97688fd270646f647522423c4f3
matt360 <matzaremba@icloud.com>
2018-08-27 10:11:32 +0200

    operator= - critical fixes in code and comments

--- 3rdEditionEffectiveC++/operator=/Main.cpp
+++ 3rdEditionEffectiveC++/operator=/Main.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <vector>
 
+// When an exception is thrown, exception - safe functions:
+// ■ Leak no resources.
+// ■ Don’t allow data structures to become corrupted.
+
 class Bitmap {
 
 };
@@ -13,30 +17,67 @@ class Widget{
 	Bitmap *pb;
 };
 
+// Here’s an implementation of operator= that looks reasonable on the
+// surface but is unsafe in the presence of assignment to self. (It’s also
+// not exception - safe, but we’ll deal with that in a moment.)
+Widget& Widget::operator=(const Widget& rhs)
+{
+	delete pb;
+	pb = new Bitmap(*rhs.pb);
+	return *this;
+	// The self - assignment problem here is that inside operator=, *this (the
+	// target of the assignment) and rhs could be the same object. When they
+	// are, the delete not only destroys the bitmap for the current object, it
+	// destroys the bitmap for rhs, too. At the end of the function, the Widget
+	// — which should not have been changed by the assignment to self —
+	// finds itself holding a pointer to a deleted object! Probably -
+	// C++ implementations are permitted to change the value of a deleted pointer
+	// (e.g., to null or some other special bit pattern), but I am unaware of any that do.
+}
+
 // The traditional way to prevent this error is to check for assignment to
 // self via an identity test at the top of operator=:
 Widget& Widget::operator=(const Widget& rhs)
 {
 	if (this == &rhs) return *this;   // identity test: if a self-assignment, do nothing
+
+	delete pb;
+	pb = new Bitmap(*rhs.pb);
+	return *this;
 }
 
 // Happily, making operator= exception - safe typically renders it selfassignment -
-// safe, too.As a result, it’s increasingly common to deal
-// with issues of self - assignment by ignoring them, focusing instead on
+// safe, too. As a result, it’s increasingly common to deal
+// with issues of self-assignment by ignoring them, focusing instead on
 // achieving exception safety.Item 29 explores exception safety in depth,
 // but in this Item, it suffices to observe that in many cases, a careful
-// ordering of statements can yield exception - safe(and self - assignmentsafe)
-// code.Here, for example, we just have to be careful not to delete
+// ordering of statements can yield exception-safe (and self-assignment safe)
+// code. Here, for example, we just have to be careful not to delete
 // pb until after we’ve copied what it points to :
 Widget& Widget::operator=(const Widget& rhs)
 {
-	Bitmap *pOrig = pb;
-	pb = new Bitmap(*rhs.pb);
-	delete pb;
+	Bitmap *pOrig = pb;              // remember original pb
+	pb = new Bitmap(*rhs.pb);        // point pb to a copy of rhs's bitmap
+	delete pOrig;                    // delete the original pb
+	return *this;
+	// Now, if “new Bitmap” throws an exception, pb(and the Widget it’s inside
+	// of) remains unchanged.Even without the identity test, this code handles
+	// assignment to self, because we make a copy of the original bitmap,
+	// point to the copy we made, then delete the original bitmap. It
+	// may not be the most efficient way to handle self - assignment, but it
+	// does work.
+
+	// If you’re concerned about efficiency, you could put the identity test
+	// back at the top of the function. Before doing that, however, ask yourself
+	// how often you expect self-assignments to occur, because the test
+	// isn’t free. It makes the code (both source and object) a bit bigger, and
+	// it introduces a branch into the flow of control, both of which can
+	// decrease runtime speed. The effectiveness of instruction prefetching,
+	// caching, and pipelining can be reduced, for example.
 }
 
 // An alternative to manually ordering statements in operator= to make
-// sure the implementation is both exception - and self - assignment - safe
+// sure the implementation is both exception-safe and self-assignment
 // is to use the technique known as “copy and swap.” This technique is
 // closely associated with exception safety, so it’s described in Item 29.
 // However, it’s a common enough way to write operator= that it’s worth
