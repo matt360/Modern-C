85df8181f0a580a0abde319e794b1391ded93f4d
matt360 <matzaremba@icloud.com>
2018-08-27 16:18:57 +0200

    update

--- 3rdEditionEffectiveC++/3rdEditionEffectiveC++/Playground.cpp
+++ 3rdEditionEffectiveC++/3rdEditionEffectiveC++/Playground.cpp
@@ -1,32 +1,10 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <unordered_set>
-
-bool HasPairWithSum(const std::vector<int>& data, int sum)
-{
-	std::unordered_set<int> set;  // complements
-
-	for (int value : data)
-	{
-		// check if I have seen the value
-		if (set.find(value) != set.end())
-		{
-			return true;
-		}
-
-		set.insert(sum - value);
-	}
-
-	return false;
-}
 
 int main()
 {
-	std::vector<int> a{ 1, 2, 3, 9 };
-	std::vector<int> b{ 1, 2, 3, 6 };
 
-	HasPairWithSum(a, 8) ? std::cout << "true\n" : std::cout << "false\n";
-	HasPairWithSum(b, 8) ? std::cout << "true\n" : std::cout << "false\n";
 
 	std::cin.get();
 }
