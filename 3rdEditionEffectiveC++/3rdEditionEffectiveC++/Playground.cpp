d8c4f0d894f810db8cdf9d5b63756628a174fa90
matt360 <matzaremba@icloud.com>
2018-08-29 16:21:14 +0200

    Added: EffectiveModernCpp Project

--- 3rdEditionEffectiveC++/3rdEditionEffectiveC++/Playground.cpp
+++ 3rdEditionEffectiveC++/3rdEditionEffectiveC++/Playground.cpp
@@ -4,6 +4,11 @@
 
 class Investment { }; // root class of hierarchy of investment types
 
+Investment* createInvestment(); // return ptr to dynamically allocated
+                                // object in the Investment hierarchy;
+                                // the caller must delete it
+                                // (parameters omitted for simplicity)
+
 int main()
 {
 
