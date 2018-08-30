73365ea81d94751d475b53ac9f3bc41687ebc99e
matt360 <matzaremba@icloud.com>
2018-08-30 10:32:56 +0200

    different types of initialization

--- EffectiveModernCpp/Item 7/Main.cpp
+++ EffectiveModernCpp/Item 7/Main.cpp
@@ -6,7 +6,13 @@
 
 int main()
 {
+	int x(0);              // initializer is in parentheses
 
+	int y = 0;             // initializer follows "=" (assignment)
+
+	int z{ 0 };            // initializer is in braces
+
+	int z = { 0 };         // initializer uses "=" and braces. C++ usally treats it the same as the braces-only version.
 
 
 	std::cin.get();
