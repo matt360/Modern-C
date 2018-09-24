71f61f2044625c23583245c27d6d4b30862a1871
matt360 <matzaremba@icloud.com>
2018-09-24 16:40:47 +0200

    Q114

--- CppQuiz/Q114/src/Main.cpp
+++ CppQuiz/Q114/src/Main.cpp
@@ -2,12 +2,37 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
+class C {
+public:
+	void foo() { std::cout << "A"; }
+	void foo() const { std::cout << "B"; }
+};
 
+struct S {
+	std::vector<C> v;
+	std::unique_ptr<C> u;
+	C *const p;
 
-int main() 
-{
-	
+	S()
+		: v(1)
+		, u(new C())
+		, p(u.get())
+	{}
+};
+
+int main() {
+	S s;
+	const S &r = s;
+
+	s.v[0].foo();
+	s.u->foo();
+	s.p->foo();
+
+	r.v[0].foo();
+	r.u->foo();
+	r.p->foo();
 
 	std::cin.get();
 }
