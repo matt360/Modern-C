e8444818a7b2e67c521cc61444e92b75f408240b
matt360 <matzaremba@icloud.com>
2018-09-24 10:04:35 +0200

    Q153

--- CppQuiz/Q153/src/Main.cpp
+++ CppQuiz/Q153/src/Main.cpp
@@ -7,7 +7,13 @@
 
 int main() 
 {
-	
+	// illegal - source https://dev.krzaq.cc/post/stop-assigning-string-literals-to-char-star-already/
+	char* str = "X";
+	std::cout << str;
+
+	// also illegal
+	const int qux = 42;
+	int* quux = &qux;
 
 	std::cin.get();
 }
