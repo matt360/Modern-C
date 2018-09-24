955c6fa117c01afc06aa1ca4400de3e01b200e47
matt360 <matzaremba@icloud.com>
2018-09-24 10:48:20 +0200

    Q44

--- CppQuiz/Q44/src/Main.cpp
+++ CppQuiz/Q44/src/Main.cpp
@@ -3,11 +3,23 @@
 #include <iostream>
 #include <string>
 
+struct X {
+	virtual void f() const { std::cout << "X"; }
+};
 
+struct Y : public X {
+	void f() const { std::cout << "Y"; }
+};
+
+void print(const X& x) { x.f(); }
 
 int main() 
 {
-	
+	X arr[1];
+	Y y1;
+	arr[0] = y1;
+	print(y1);
+	print(arr[0]);
 
 	std::cin.get();
 }
