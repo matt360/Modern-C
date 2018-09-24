3d0523dc7bf1f5ed0c50e3edbe5e06e160ae19a2
matt360 <matzaremba@icloud.com>
2018-09-24 14:12:00 +0200

    Q9

--- CppQuiz/Q9/src/Main.cpp
+++ CppQuiz/Q9/src/Main.cpp
@@ -3,11 +3,19 @@
 #include <iostream>
 #include <string>
 
-
+int f(int &a, int &b)
+{
+	a = 3;
+	b = 4;
+	return (a + b);
+}
 
 int main() 
 {
-	
+	int a = 1;
+	int b = 2;
+	int c = f(a, a);
+	std::cout << a << b << c;
 
 	std::cin.get();
 }
