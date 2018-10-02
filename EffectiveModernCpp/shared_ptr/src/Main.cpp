ac766836c4880037f831c679dad1961c703455ce
matt360 <matzaremba@icloud.com>
2018-10-02 16:29:01 +0200

    Smart Pointers - shared_ptr

--- EffectiveModernCpp/shared_ptr/src/Main.cpp
+++ EffectiveModernCpp/shared_ptr/src/Main.cpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <vector>
 
 
 class Widget {};
 
-auto loggingDel = [](Widget *pw)
+auto loggingDel = [](Widget *pw)  // custom deleter
 {
 	auto makeLogEntry = [](Widget *pw) 
 	{ 
@@ -15,6 +16,19 @@ auto loggingDel = [](Widget *pw)
 	};
 };
 
+std::unique_ptr<Widget, decltype(loggingDel)>  // deleter type is part of ptr type
+upw(new Widget, loggingDel);
+
+std::shared_ptr<Widget>                        // deleter type is not part of ptr type
+spw(new Widget, loggingDel);
+
+auto customDeleter1 = [](Widget *pw) { std::cout << "customDeleter1"; };  // custom deleters,
+auto customDeleter2 = [](Widget *pw) { std::cout << "customDeleter2"; };  // each with a different type
+
+std::shared_ptr<Widget> pw1(new Widget, customDeleter1);
+std::shared_ptr<Widget> pw2(new Widget, customDeleter2);
+
+std::vector<std::shared_ptr<Widget>> vpw{ pw1, pw2 };
 
 int main() 
 {
