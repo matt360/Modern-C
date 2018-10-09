fba69392630dd740dfec236f260572817afe1ea9
zarembamat <mateusz.zaremba@bistudio.com>
2018-10-09 14:01:49 +0200

    Q148

--- CppQuiz/Q148/Main.cpp
+++ CppQuiz/Q148/Main.cpp
@@ -5,6 +5,8 @@ volatile int a;
 int main() {
 	std::cout << (a + a);
 
+
+
 	/*
 	The issue here is not the missing initializer of the variable a - it will implicitly be initialized to 0 here. But the issue is the access to a twice without sequencing between the accesses. According to [intro.execution]§4.6¶14, accesses of volatile glvalues are side-effects and according to [intro.execution]§4.6¶17 these two unsequenced side-effects on the same memory location results in undefined behavior.
 	*/
