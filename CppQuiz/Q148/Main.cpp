3eb539e9f299f91b5ddcd8270776661db53cb413
zarembamat <mateusz.zaremba@bistudio.com>
2018-10-09 14:26:44 +0200

    qcl https://github.com/matt360/FPSProject.git

--- CppQuiz/Q148/Main.cpp
+++ CppQuiz/Q148/Main.cpp
@@ -6,6 +6,8 @@ int main() {
 	std::cout << (a + a);
 
 
+	union { int a; int b; };
+
 
 	/*
 	The issue here is not the missing initializer of the variable a - it will implicitly be initialized to 0 here. But the issue is the access to a twice without sequencing between the accesses. According to [intro.execution]§4.6¶14, accesses of volatile glvalues are side-effects and according to [intro.execution]§4.6¶17 these two unsequenced side-effects on the same memory location results in undefined behavior.
