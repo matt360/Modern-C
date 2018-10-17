04a028dfed9f67f9c09a5c9d107cfaf174d74951
zarembamat <mateusz.zaremba@bistudio.com>
2018-10-17 15:08:32 +0200

    Q184, Q29, Q5

---
+++ CppQuiz/Q29/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct A
+{
+	A() { foo(); }
+	virtual ~A() { foo(); }
+	virtual void foo() { std::cout << "1"; }
+	void bar() { foo(); }
+};
+
+struct B : public A
+{
+	virtual void foo() { std::cout << "2"; }
+};
+
+
+int main()
+{
+	{
+		B b;
+		b.bar();
+	}
+
+	std::cin.get();
+}
---
+++ CppQuiz/Q5/src/Main.cpp
@@ -0,0 +1,35 @@
+// Created by Mateusz Zaremba
+
+#include <iostream>
+#include <string>
+
+
+struct A {
+	A() { std::cout << "A"; }
+};
+struct B {
+	B() { std::cout << "B"; }
+};
+
+class C {
+public:
+	C() : a(), b() {}
+
+private:
+	B b;
+	A a;
+};
+
+int main()
+{
+	C();
+
+	// output:
+	// BA
+	// 
+	/*
+	 *The initialization order of member variables is determined by their order of declaration, not their order in the initialization list.
+	 **/
+
+	std::cin.get();
+}
