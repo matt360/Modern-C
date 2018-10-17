6cc608036164b8c97a36a4923459e4065633bcaf
zarembamat <mateusz.zaremba@bistudio.com>
2018-10-17 15:46:57 +0200

    Q124, Q129

---
+++ CppQuiz/Q129/src/Main.cpp
@@ -0,0 +1,15 @@
+// Created by Mateusz Zaremba
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+int main() 
+{
+	std::vector<char> delimiters = { ",", ";" };
+	std::cout << delimiters[0];
+
+	std::cin.get();
+}
