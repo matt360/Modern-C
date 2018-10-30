96bb75d4708acc14172a6b80a72c509a48d532b6
zarembamat <mateusz.zaremba@bistudio.com>
2018-10-30 14:16:33 +0100

    working post prefix with many incrementations

--- MoreEffectiveCpp/MoreEffectiveCpp/src/Main.cpp
+++ MoreEffectiveCpp/MoreEffectiveCpp/src/Main.cpp
@@ -3,11 +3,34 @@
 #include <iostream>
 #include <string>
 
+struct C
+{
+	C& operator++(); // prefix
+	C& operator++(int); // postfix
 
+
+	int i = 0;
+};
+
+C& C::operator++()
+{
+	this->i = this->i + 1;
+	return *this;
+}
+
+
+C& C::operator++(int)
+{
+	this->i = this->i + 1;
+	return *this;
+}
 
 int main() 
 {
-	
+	C c;
+	c++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++;
+
+	std::cout << c.i;
 
 	std::cin.get();
 }
