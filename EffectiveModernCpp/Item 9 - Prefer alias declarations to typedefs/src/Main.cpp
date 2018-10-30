6ec65fc24f7ee5cd0a5edc8e911b38deddd13db8
zarembamat <mateusz.zaremba@bistudio.com>
2018-10-30 15:09:48 +0100

    EffectiveModernCpp - Item 9

---
+++ EffectiveModernCpp/Item 9 - Prefer alias declarations to typedefs/src/Main.cpp
@@ -0,0 +1,29 @@
+// Created by Mateusz Zaremba
+
+#include <iostream>
+#include <string>
+#include <list>
+
+class Widget {};
+
+// alias version
+template<typename T>
+using MyAllocListA = std::list<T, MyAlloc<T>>;	// MyAllocListA<T> is synonym for
+												// std::list<T, MyAlloc<T>> 
+
+MyAllocListA<Widget> lwA;						// client code
+
+// typedef version
+template<typename T>							// MyAllocListTD::type is synonym for
+struct MyAllocListTD {							// std::list<T, MyAlloc<T>>
+	typedef std::list<T, MyAlloc<T>> type;
+};
+
+MyAllocListTD<Widget> lwTD;						// client code
+
+int main() 
+{
+	
+
+	std::cin.get();
+}
